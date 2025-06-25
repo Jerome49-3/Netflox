69c622452900aaa5ebfb653dc44f4ce307ede10c
jerome <contact@teilienCard.com>
2025-06-26 00:13:36 +0200

    change path to logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './assets/css/App.css'
 import Header from './components/header/Header'
-import logoNetflix from "./assets/images/netfalshLogo.png"
+import logoNetflix from "./assets/images/netfalsh.svg"
 import Main from './components/main/Main'
 import Footer from './components/footer/Footer'
 
